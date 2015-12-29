49ed353649d3e0b4738182eb904d531711ddc97c
Joseph Meyer <josephedwardmeyer@gmail.com>
2015-12-28 22:55:03 -0600

    test

---
+++ 2015-05-16/2015-05-16/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _2015_05_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get equation, starting number, and iteration count
+            Console.WriteLine("Please enter tranformation equation:");
+            string[] transformation = Console.ReadLine().Split(' ');
+            Console.WriteLine("Enter starting term:");
+            int startingTerm = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ender number of iterations:");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            // List first iteration and then call recursive function
+            Console.WriteLine("Term 0: " + startingTerm);
+            recurse(transformation, startingTerm, iterations, 0);
+
+            Console.ReadLine();
+        }
+
+        static void recurse(string[] transform, int term, int count, int termNum)
+        {
+            // We are done
+            if (termNum == count)
+            { return; }
+
+            // Loop for each part of transformation equation
+            for (int i = 0; i < transform.Length; i++)
+            {
+                // Get operator and value
+                string op = transform[i].Substring(0, 1);
+                int val = Convert.ToInt32(transform[i].Substring(1));
+
+                // Calculate
+                switch (op)
+                {
+                    case "+":
+                        term += val;
+                        break;
+                    case "-":
+                        term -= val;
+                        break;
+                    case "*":
+                        term *= val;
+                        break;
+                    case "/":
+                        term /= val;
+                        break;
+                }
+            }
+
+            // Output
+            Console.WriteLine(string.Format("Term {0}: {1}", ++termNum, term));
+
+            // Call self
+            recurse(transform, term, count, termNum);
+        }
+    }
+}
